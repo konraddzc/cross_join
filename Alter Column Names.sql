cd8cf2e198b04e065fd2aad1fb985a17e8ea18ab
konraddzc <konrad.dzc@gmail.com>
2025-09-22 14:27:57 +0100

    Add files via upload

---
+++ Alter Column Names.sql
@@ -0,0 +1,26 @@
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Purchase-date" TO purchase_date;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Date" TO ddmmyyyy;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Time" TO "time";
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Year" TO "year";
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Month" TO "month";
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN date_1 TO day_of_month;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Day" TO day_of_week;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Special Day" TO special_day;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Online Sale Offers " TO online_sale_offers;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN gender TO isfemale;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Customer ID" TO customer_id;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Product-Name" TO product_name;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Item-Status" TO item_status;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Item Price" TO item_price;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Shipping-Price" TO shipping_price;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Ship-City" TO ship_city;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Ship-State" TO ship_state;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Ship-Postal-Code" TO ship_postal_code;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Total_
+amount" TO total_amount;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Author " TO author;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Publication" TO "publication";
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Profit Percentage
+ (%)" TO profit_percentage;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Profit (INR)" TO profit_inr;
+ALTER TABLE all_2509.cross_join_temp RENAME COLUMN "Cost Price" TO cost_price;
